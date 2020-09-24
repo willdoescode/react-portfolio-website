5d819ad3bd82e621d836ccbfec105768375bbac8
Will Lane <williamlane923@gmail.com>
2020-09-24 08:54:30 -0700

    added more cool typewriter effects

--- src/components/routes/About.js
+++ src/components/routes/About.js
@@ -5,28 +5,43 @@ import Typewriter from 'typewriter-effect';
 export default function About(callback, thisArg) {
   return (
     <>
-      <Typewriter classNane="yay"
-        options={{
-          loop: true
-        }}
-        onInit={(typewriter) => {
-          typewriter.typeString('Will Lane, ')
-            .pauseFor(250)
-            .start();
-          typewriter.typeString('Python developer.')
-            .pauseFor(250)
-            .deleteChars(17)
-            .start();
-          typewriter.typeString('Vue developer.')
-            .pauseFor(250)
-            .deleteChars(14)
-            .start();
-          typewriter.typeString('React developer.')
-            .pauseFor(250)
-            .deleteChars(16)
-            .start();
-        }}
-      />
+      <div className="container">
+        <Typewriter classNane="yay"
+                    options={{
+                      loop: true
+                    }}
+                    onInit={(typewriter) => {
+                      typewriter.typeString('Will Lane, ')
+                        .pauseFor(250)
+                        .changeDeleteSpeed(2)
+                        .start();
+                      typewriter.typeString('Python developer.')
+                        .pauseFor(250)
+                        .changeDeleteSpeed(2)
+                        .deleteChars(17)
+                        .pauseFor(90)
+                        .start();
+                      typewriter.typeString('Vue developer.')
+                        .pauseFor(250)
+                        .changeDeleteSpeed(2)
+                        .deleteChars(14)
+                        .pauseFor(90)
+                        .start();
+                      typewriter.typeString('React developer.')
+                        .pauseFor(250)
+                        .changeDeleteSpeed(2)
+                        .deleteChars(16)
+                        .pauseFor(90)
+                        .start();
+                      typewriter.typeString('Node developer.')
+                        .pauseFor(250)
+                        .changeDeleteSpeed(2)
+                        .deleteChars(15)
+                        .pauseFor(90)
+                        .start();
+                    }}
+        />
+      </div>
     </>
   )
 }
