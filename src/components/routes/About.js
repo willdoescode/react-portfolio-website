5c9f3a82817a6448c7eebfc268bb5588d1fdc39a
Will Lane <williamlane923@gmail.com>
2020-09-24 11:08:34 -0700

    commit what i have

--- src/components/routes/About.js
+++ src/components/routes/About.js
@@ -2,46 +2,86 @@ import React, { useState } from "react";
 import '../../styles/About.css';
 import Typewriter from 'typewriter-effect';
 
-export default function About(callback, thisArg) {
+export default function About() {
+  const date = new Date()
+  const getAge = () => {
+    let year = date.getFullYear()
+    if (date.getMonth() < 8 && date.getDay() < 23) {
+      year--
+    }
+    return year - 2006
+  }
   return (
-    <>
+    <div className='background'>
       <div className="container">
-        <Typewriter classNane="yay"
-                    options={{
-                      loop: true
-                    }}
-                    onInit={(typewriter) => {
-                      typewriter.typeString('Will Lane, ')
-                        .pauseFor(250)
-                        .changeDeleteSpeed(2)
-                        .start();
-                      typewriter.typeString('Python developer.')
-                        .pauseFor(250)
-                        .changeDeleteSpeed(2)
-                        .deleteChars(17)
-                        .pauseFor(90)
-                        .start();
-                      typewriter.typeString('Vue developer.')
-                        .pauseFor(250)
-                        .changeDeleteSpeed(2)
-                        .deleteChars(14)
-                        .pauseFor(90)
-                        .start();
-                      typewriter.typeString('React developer.')
-                        .pauseFor(250)
-                        .changeDeleteSpeed(2)
-                        .deleteChars(16)
-                        .pauseFor(90)
-                        .start();
-                      typewriter.typeString('Node developer.')
-                        .pauseFor(250)
-                        .changeDeleteSpeed(2)
-                        .deleteChars(15)
-                        .pauseFor(90)
-                        .start();
-                    }}
+        <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter.pasteString('$ ')
+              typewriter.typeString('npx create-react-app will-lane')
+                .pauseFor(250)
+                .changeDeleteSpeed(2)
+                .deleteAll(-2000000000)
+                .start();
+              typewriter.pasteString('$ ')
+              typewriter.typeString('cd will-lane/')
+                .pauseFor(250)
+                .changeDeleteSpeed(-200000000)
+                .pauseFor(500)
+                .deleteAll(-20000000)
+                .start();
+              typewriter.pasteString('will-lane$ ')
+                .pauseFor(250)
+                .changeDeleteSpeed(2)
+                .start();
+              typewriter.typeString('npm i -D set-age')
+                .pauseFor(250)
+                .changeDeleteSpeed(-200000000)
+                .pauseFor(500)
+                .deleteChars(16)
+                .start();
+              typewriter.typeString(`set-age ${getAge()}`)
+                .pauseFor(250)
+                .changeDeleteSpeed(-200000000)
+                .pauseFor(500)
+                .deleteChars(10)
+                .start();
+              typewriter.typeString('python --developer')
+                .pauseFor(250)
+                .changeDeleteSpeed(-200000000)
+                .pauseFor(500)
+                .deleteChars(18)
+                .start();
+              typewriter.typeString('vue --developer')
+                .pauseFor(250)
+                .changeDeleteSpeed(-200000000)
+                .pauseFor(500)
+                .deleteChars(15)
+                .start();
+              typewriter.typeString('react --developer')
+                .pauseFor(250)
+                .changeDeleteSpeed(-2000000000)
+                .pauseFor(500)
+                .deleteChars(17)
+                .start();
+              typewriter.typeString('node --developer')
+                .pauseFor(250)
+                .changeDeleteSpeed(-2000000000)
+                .pauseFor(500)
+                .deleteChars(16)
+                .start();
+              typewriter.typeString('cool --developer 😎')
+                .pauseFor(250)
+                .changeDeleteSpeed(-2000000000)
+                .pauseFor(500)
+                .deleteAll(-2000000000)
+                .start();
+            }}
         />
       </div>
-    </>
+    </div>
   )
 }
